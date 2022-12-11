dd1e174325c0a965f4d48268fa43e649494d8187
K.Tensh Kumar <tenshkumarkkt@gmail.com>
2022-12-12 02:03:20 +0530

    Firebase-DB-Connection-Done

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // importing requirements
 const express = require("express")
 const cookieParser = require("cookie-parser")
+const admin = require("firebase-admin");
 
 //Routers
 
@@ -22,6 +23,25 @@ app.use(express.json())
 app.use(express.urlencoded({
   extended: true
 }))
+
+// firebase
+// initializeApp
+const serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+// firebase firestore database service init
+const db = admin.firestore()
+// collection Reference
+const colRef = db.collection('gps-coordinates')
+// get collection data
+colRef.get()
+  .then((snapshot)=>{
+    console.log(snapshot)
+  })
+
 
 const PORT = process.env.PORT || 8888
 
